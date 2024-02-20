58a6cf72f913f6046d6d35c0675be35b150996c2
Aaron <berman.aaron@gmail.com>
2024-02-20 18:47:43 +0100

    Update xml_queries.sql

--- SQL/xml_queries.sql
+++ SQL/xml_queries.sql
@@ -1,2 +1,29 @@
+
+/* Generic xmlfile query */
 SELECT CAST(MY_XML AS xml)
   FROM OPENROWSET(BULK 'C:\filename.xml', SINGLE_BLOB) as T(MY_XML)
+
+
+/* procedure to import xml file */
+CREATE PROCEDURE [dbo].[importXML]
+(
+  @applicationName nvarchar(50),
+  @toolName nvarchar(50),
+  @fileName nvarchar(max)
+)
+AS
+BEGIN
+  DECLARE @sql nvarchar(max);
+
+  SET @sql = N'INSERT INTO os.xmlFile
+                SELECT
+                      OSApplication = @applicationName,
+                      OSTool = @toolName,
+                      OSxml = CAST(MY_XML AS xml)
+                FROM OPENROWSET(BULK ''' + @fileName + ''', SINGLE_BLOB) AS T(MY_XML)';
+
+  EXEC sp_executesql @sql, N'@applicationName nvarchar(50), @toolName nvarchar(50)',
+                       @applicationName, @toolName;
+END;
+
+GO
